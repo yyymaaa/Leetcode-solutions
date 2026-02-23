b030bef1c0227f92bc5abde041b50010a3ab429c
yyymaaa <ojukaamy43@gmail.com>
2026-02-23 12:10:32 +0300

    Cyclic Rotation on Arrays

---
+++ codility_cyclicRotation.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+
+// A = [3, 8, 9, 7, 6] when rotated K=1 times, you get A = [6, 3, 8, 9, 7]
+// GOAL: Rotate Array A, K times
+
+std::vector<int> solution(std::vector<int> &A, int K) {
+    int N = A.size();
+    if (A.empty()) return A;
+
+    K = K % N; // saves the computer form doing useless laps
+    if (K == 0) return A;
+
+    //LONG SOLUTION
+    // for (int rotation = 0; rotation < K; rotation++) {
+    //     int lastElement = A[N-1];
+
+    //     for (int i = N - 1; i > 0; i--) {
+    //         A[i] = A[i - 1];
+    //     }
+
+    //     A[0] lastElement;
+
+    // }
+
+
+    //SHORTCUT
+    std::vector<int> result(N);
+    for (int i = 0; i < N; i++) {
+        int newPosition = (i + K) % N;
+        result[newPosition] = A[i];
+    }
+
+ 
+
+    return result;
+}
+
+
+int main() {
+    // Example test case
+    std::vector<int> A = {3, 8, 9, 7, 6};
+    int K = 3;
+
+    std::vector<int> rotatedA = solution(A, K);
+
+   
+    std::cout << "Rotated Array: [ ";
+    for (int x : rotatedA) {
+        std::cout << x << " ";
+    }
+    std::cout << "]" << std::endl;
+
+    return 0;
+}
+
